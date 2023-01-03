1709f5ff59074e17f2597791f34f6d17eea30b1c
Manoel <jiujitsu30@gmail.com>
2023-01-03 16:49:51 -0300

    #Arquivos

--- logica_prog_basica/32_exercicios.py
+++ logica_prog_basica/32_exercicios.py
@@ -4,15 +4,59 @@ informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
 
+entrada = input('Digite um numero inteiro: ')
+
+try:
+    
+    entrada_int = int(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'impar'
+
+    if par_impar:
+        par_impar_texto = 'par'
+
+    print(f'Este numero {entrada_int} é {par_impar_texto}')
+    
+except:
+    print('Isso não é um número inteiro!')
+
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
+print('--------------------------------------')
+quantas_hora = input('Que horas são: ')
+
+try:
+
+    hora = int(quantas_hora)
+
+    if hora >= 0 and hora <= 11:
+        print(f'Bom dia!')
+    elif hora >= 12 and hora <= 17:
+        print(f'Boa tarde!')
+    elif hora >= 18 and hora <= 23:
+        print(f'Boa noite!')
+    else:
+        print('Não conheço esta hora.')
+except:
+    print('Digite apenas números inteiros!')
+
 
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+print('--------------------------------------')
+nome = input('Digite seu primeiro nome: ')
 
+tamanho_nome = len(nome)
+
+if tamanho_nome <= 4:
+    print(f'Seu nome é curto.')
+elif tamanho_nome >= 5 and tamanho_nome <= 6:
+    print('Seu nome é normal.')
+else:
+    print('Seu nome é muito grande.')
